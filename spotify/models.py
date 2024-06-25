f2df69885c69a66f2edbf85b7fe7f88c50d714ab
Nuwagaba Victor <nuwagabavictor197@gmail.com>
2024-06-25 22:09:19 +0300

    models finished

--- spotify/models.py
+++ spotify/models.py
@@ -1,3 +1,35 @@
 from django.db import models
 
 # Create your models here.
+class Artist(models.Model):
+    Name = models.CharField(max_length=100)
+    bio = models.TextField(blank=True, null=True)
+
+    def __str__(self):
+        return self.Name
+
+class Album(models.Model):
+    title = models.CharField(max_length=50)
+    artist = models.ForeignKey(Artist, on_delete=models.CASCADE)
+    release_date = models.DateField()
+
+    def __str__(self):
+        return self.title
+    
+class song(models.Model):
+    title = models.CharField(max_length=100)
+    album = models.ForeignKey(Album, on_delete=models.CASCADE)
+    artist = models.ForeignKey(Artist, on_delete=models.CASCADE)
+    duration = models.DurationField()
+    avalability = models.BooleanField(default=True)
+
+    def __str__(self):
+        return self.title
+    
+class user(models.Model):
+    name = models.CharField(max_length=100)
+    email = models.EmailField(unique=True)
+    phone_number = models.CharField(max_length=50, blank=True, null=True)
+
+    def __str__(self):
+        return f"{self.name},{self.email}"
