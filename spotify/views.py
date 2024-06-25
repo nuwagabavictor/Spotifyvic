3ccb43d01a4210e74b61d098dae54c91c880accd
Nuwagaba Victor <nuwagabavictor197@gmail.com>
2024-06-25 22:40:20 +0300

    started on views

--- spotify/views.py
+++ spotify/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
+from .models import Artist, Album, song, user
 
 # Create your views here.
+def artist_list(request):
+    artists = Artist.objects.all()
+    context = {
+        'artists': artists
+    }
+    return render(request, 'spotify/artist_list.html', context)
